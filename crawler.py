0c7c6b09c805691b9d26c346075b0d1e50ba0d73
Byeori-Kim <99kbr11@gmail.com>
2021-03-02 11:52:46 +0900

    web crawling

--- crawler.py
+++ crawler.py
@@ -3,18 +3,19 @@ import pandas as pd
 import requests
 
 count = 0
-with open("C:\work\INFOSEC_CERlT_PA_ExportReport2021_02_25_11_33_17.csv") as file:
-    reader = pd.read_csv(file)
-    url_col = reader['Infosec URL']
-    for row in url_col:
-        for count in range(0, 10):
-            count = count + 1
-            page = requests.get(row)
-            print(page.content)
-            # soup = BeautifulSoup(page.content, "html.parser")
+# with open("list.csv") as file:
+#     reader = pd.read_csv(file)
+#     url_col = reader['Infosec URL']
+#     for row in url_col:
+#         for count in range(0, 10):
+#             count = count + 1
+#             page = requests.get(row)
+#             soup = BeautifulSoup(page.content, "html.parser")
 
-# URL="https://infosec.cert-pa.it/analyze/f15c38ca92588c369b59dd4bcdba4523.html"
-# page = requests.get(URL)
-# soup = BeautifulSoup(page.content, "html.parser")
-# eee = soup.find('div', "co-sm-6")
-# print(eee)
+URL="https://infosec.cert-pa.it/analyze/36e7f674ff9146b0c6555ecfe9a124fc.html"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+tab = soup.find_all('div', "col-sm-6")
+Packer = tab[3].find('tbody').find_all('td')
+for packers in Packer:
+    print(packers)
